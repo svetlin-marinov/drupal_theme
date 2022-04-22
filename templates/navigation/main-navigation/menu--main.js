8b63c503f3e7ceb23ae6b2f6783b465e1a1a33f3
Svetlin Marinov <svetlin.marinov@ffwagency.com>
2022-04-22 17:59:07 +0300

    FECI-30: Header, footer, main menu.

--- templates/navigation/main-navigation/menu--main.js
+++ templates/navigation/main-navigation/menu--main.js
@@ -4,30 +4,33 @@
 
       /* Main Navigation Menu */
       const menuMain = $('.menu--main', context);
-      const closeMenu = $('.menu-close-icon', context);
-      const hamburgerToggle = $('.hamburger-menu', context);
+      const mobileMenuToggle = $('.mobile-menu-toggle', context);
       const menuDropdown = $('.menu--main--dropdown', menuMain);
       const responsiveDropdownTrigger = $('.responsive-dropdown-trigger', menuMain);
       const pageBody = $(document.body);
 
       /* Toggle hamburger menu */
-      hamburgerToggle.on('click', function () {
-        addResponsiveMenuStyles();
-      });
-
-      closeMenu.on('click', function () {
-        removeResponsiveMenuStyles();
+      mobileMenuToggle.on('click', function () {
+        if($(this).hasClass('mobile-menu-open')) {
+          removeResponsiveMenuStyles();
+        }
+        else {
+          console.log(mobileMenuToggle);
+          addResponsiveMenuStyles();
+        }
       });
 
       /* Toggle styles for the navigation menu on small and medium sized screens */
       function addResponsiveMenuStyles() {
         pageBody.addClass('mobile-menu-open');
+        mobileMenuToggle.addClass('mobile-menu-open');
       }
 
       function removeResponsiveMenuStyles() {
         pageBody.removeClass('mobile-menu-open');
         responsiveDropdownTrigger.removeClass('dropdown-arrow-down');
         menuDropdown.removeClass('visible');
+        mobileMenuToggle.removeClass('mobile-menu-open');
       }
 
       /* Trigger dropdown menu from mobile (on click) */
@@ -35,7 +38,7 @@
         var triggerLink = $(this);
         triggerLink.once().on('click', function () {
           triggerLink.toggleClass('dropdown-arrow-down');
-          var subMenuDropdown = triggerLink.next('.menu--main--dropdown');
+          let subMenuDropdown = triggerLink.next('.menu--main--dropdown');
           subMenuDropdown.toggleClass('visible');
         });
       });
