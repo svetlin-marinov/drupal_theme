7ef83213a2eea9f6f9456814b0a0e4144d818d1a
Svetlin Marinov <svetlin.marinov@ffwagency.com>
2022-04-28 16:37:39 +0300

    FECI-30: Search pages.

---
+++ templates/content/page-search/page-search.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * Make search field sticky on page scroll.
+ */
+
+// (function ($, Drupal) {
+//   Drupal.behaviors.stickySearch = {
+//     attach: function (context) {
+//       let headerBelowElement = $('#header').get(0);
+//       let viewFilterSearch = $('.search-bar-styles', context);
+//       let prevRatio = 0.0;
+
+//       function createObserver() {
+//         let observer;
+
+//         observer = new IntersectionObserver(handleIntersect);
+//         observer.observe(headerBelowElement);
+//       }
+
+//       function handleIntersect(entries) {
+//         entries.forEach((entry) => {
+//           if (entry.intersectionRatio > prevRatio) {
+//             viewFilterSearch.removeClass('sticky');
+//           } else {
+//             // entry.intersectionRatio == 0
+//             viewFilterSearch.addClass('sticky');
+//           }
+
+//           prevRatio = entry.intersectionRatio;
+//         });
+//       }
+
+//       createObserver();
+//     },
+//   };
+// })(jQuery, Drupal);
